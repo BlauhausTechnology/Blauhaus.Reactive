6dd1ce8ef4abe1763dd1818fa1af717843d62aec
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-03 16:39:58 +0200

    no message

---
+++ src/Blauhaus.Reactive.Abstractions/Scheduler/IReactiveScheduler.cs
@@ -0,0 +1,13 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Blauhaus.Reactive.Abstractions.Scheduler
+{
+    public interface IReactiveScheduler
+    {
+        IScheduler CurrentThread { get; }
+        IScheduler Immediate { get; }
+        IScheduler NewThread { get; }
+        IScheduler ThreadPool { get; }
+        IScheduler TaskPool { get; } 
+    }
+}
